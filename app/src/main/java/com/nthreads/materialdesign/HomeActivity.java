fe98f4e1942c1d51da66eca5a1461d1346a89d12
Nauman Zubair <nthreadsblog@gmail.com>
2015-09-18 19:18:57 +0500

    Actionbar Items and tool bar customization

--- app/src/main/java/com/nthreads/materialdesign/HomeActivity.java
+++ app/src/main/java/com/nthreads/materialdesign/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.nthreads.materialdesign;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,6 +15,8 @@ import android.widget.Toast;
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +24,83 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
 
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Set Drawerlayout switch indicator that the icon leftmost Toolbar
+        ActionBarDrawerToggle mActionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.lbl_open, R.string.lbl_close);
+        mActionBarDrawerToggle.syncState();
+        drawerLayout.setDrawerListener(mActionBarDrawerToggle);
+
+        float scale =  getApplicationContext().getResources().getDisplayMetrics().density;
+        Toast.makeText(getApplicationContext(), getDensity(scale), Toast.LENGTH_LONG).show();
+
+        // Write their own method of setting operation NavigationView the menu item to be executed after being selected
+        onNavigationViewMenuItemSelected(navigationView);
+
+    }
+
+    /**
+     Operation * Set NavigationView the menu after the item is selected to be executed
+     *
+     * @param navView
+     */
+    private void onNavigationViewMenuItemSelected (NavigationView navView) {
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                String msgString = "";
+
+                switch (menuItem.getItemId()) {
+                    case R.id.navItemHome:
+                        msgString = (String) menuItem.getTitle();
+                        break;
+                    case R.id.navItemProfile:
+                        msgString = (String) menuItem.getTitle();
+                        break;
+                    case R.id.navItemInBox:
+                        msgString = (String) menuItem.getTitle();
+                        break;
+                    case R.id.navItemSetting:
+                        msgString = (String) menuItem.getTitle();
+                        break;
+                    case R.id.navItemLogout:
+                        msgString = (String) menuItem.getTitle();
+                        break;
+                }
+
+                // Menu item clicked on, and close Drawerlayout
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+
+                // Android-support-design package is compatible with the newly added Toast a similar control.
+                //SnackbarUtil.show(mViewPager, msgString, 0);
+
+                return  true ;
+            }
+        });
+    }
+
+    private String getDensity(float scale) {
+        if (scale == 0.75f)
+            return "Low Density : ldpi";
+        if (scale == 1.0f)
+            return "Medium Density : mdpi";
+        if (scale == 1.5f)
+            return "High Density : hdpi";
+        if (scale == 2.0f)
+            return "Extra High Density : xhpi";
+        if (scale == 3.0f)
+            return "Double Extra High Density : xxhpi";
+        if (scale == 4.0f)
+            return "Triple Extra High Density : xxxhpi";
+
+        else
+            return "Not detected :  scale : " + scale;
     }
 
     @Override
